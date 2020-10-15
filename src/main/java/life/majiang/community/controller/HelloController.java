cbed9eaafd2a7e699aa14449b2981d53535008b0
paulro1991 <JimmyLaw1291@gmail.com>
2020-10-15 18:49:32 +0800

    rename package name

--- src/main/java/life/majiang/community/controller/HelloController.java
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -1,9 +1,8 @@
-package life.majiang.community.community.controller;
+package life.majiang.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
